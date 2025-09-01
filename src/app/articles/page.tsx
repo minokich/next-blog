fb96db7864f4edcb474f258a5cc064043f1e8afb
minokich <mino0525dev@gmail.com>
2025-09-01 18:07:43 +0900

    any使用箇所修正

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,6 +1,30 @@
 import { getArticles } from "@/lib/microcms";
 import Link from "next/link";
 
+type article = {
+    id: string,
+    createdAt: string,
+    updatedAt: string,
+    publishedAt: string,
+    revisedAt: string,
+    title: string,
+    content: string,
+    eyecatch?: {
+        url: string,
+        height: number,
+        width: number
+    },
+    category: {
+        id: string,
+        createdAt: string,
+        updatedAt: string,
+        publishedAt: string,
+        revisedAt: string,
+        name: string
+    }
+}
+
+
 export default async function HomePage() {
     const articles = await getArticles();
 
@@ -8,7 +32,8 @@ export default async function HomePage() {
         <main>
             <h1>ブログ一覧</h1>
             <ul>
-                {articles.map((a: any) => (
+
+                {articles.map((a: article) => (
                     <li key={a.id}>
                         <Link href={`/articles/${a.id}`}>{a.title}</Link>
                     </li>
