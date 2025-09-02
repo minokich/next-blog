5297cff4236926591516a3d767dad0d7887908a4
minokich <mino0525dev@gmail.com>
2025-09-02 22:50:31 +0900

    パンくずリストStorybook作成

---
+++ src/components/CustomBreadcrumbs/CustomBreadcrumbs.stories.ts
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/nextjs-vite'
+import CustomBreadcrumbs from "./CustomBreadcrumbs";
+
+const meta: Meta<typeof CustomBreadcrumbs> = {
+    component: CustomBreadcrumbs,
+};
+export default meta;
+
+export const Default: StoryObj = {
+    args: {
+        links: [{ href: "/", label: "TOP" }, { href: "/", label: "前のページ" }],
+        curreentLabel: "現在ページ"
+    },
+};
