d0eee85c6e4db98a480414da5f1bbb7e65b433f6
minokich <mino0525dev@gmail.com>
2025-09-03 10:29:28 +0900

    SSG化

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,42 +2,45 @@ import { getArticles } from "@/lib/microcms";
 import Link from "next/link";
 
 type article = {
-    id: string,
-    createdAt: string,
-    updatedAt: string,
-    publishedAt: string,
-    revisedAt: string,
-    title: string,
-    content: string,
-    eyecatch?: {
-        url: string,
-        height: number,
-        width: number
-    },
-    category: {
-        id: string,
-        createdAt: string,
-        updatedAt: string,
-        publishedAt: string,
-        revisedAt: string,
-        name: string
-    }
-}
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  eyecatch?: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+};
 
+export const revalidate = 60;
 
-export default async function HomePage() {
-    const articles = await getArticles();
+const ArticlesPage = async () => {
+  const articles = await getArticles();
 
-    return (
-        <main>
-            <h1>ブログ一覧</h1>
-            <ul>
-                {articles.map((a: article) => (
-                    <li key={a.id}>
-                        <Link href={`/articles/${a.id}`}>{a.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
-}
+  return (
+    <main>
+      <h1>ブログ一覧</h1>
+      <ul>
+        {articles.map((a: article) => (
+          <li key={a.id}>
+            <Link href={`/articles/${a.id}`}>{a.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default ArticlesPage;
