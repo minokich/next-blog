17d7a11a143d3ad92115ce3fecd6e4e4c205c253
minokich <mino0525dev@gmail.com>
2025-09-03 15:13:02 +0900

    アイキャッチをコンポーネント化

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,10 +1,10 @@
 import { getArticle, getArticles } from "@/lib/microcms";
-import Image from "next/image";
 
 import Article from "@/components/Article/Article";
 import PageLayout from "@/components/PageLayout/PageLayout";
 import CustomBreadcrumbs from "@/components/CustomBreadcrumbs/CustomBreadcrumbs";
 import ArticleTitle from "@/components/ArticleTitle/ArticleTitle";
+import EyeCatchImage from "@/components/EyeCatchImage/EyeCatchImage";
 
 type ArticlePageProps = {
   params: {
@@ -32,9 +32,7 @@ const ArticlePage = async ({ params }: ArticlePageProps) => {
       <CustomBreadcrumbs links={links} curreentLabel={article.title} />
       <PageLayout>
         <ArticleTitle>{article.title}</ArticleTitle>
-        {article.eyecatch && (
-          <Image src={article.eyecatch.url} alt={article.title} width={800} height={400} />
-        )}
+        {article.eyecatch && <EyeCatchImage src={article.eyecatch.url} alt={article.title} />}
         <Article content={article.content} />
       </PageLayout>
     </>
---
+++ src/components/EyeCatchImage/EyeCatchImage.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import styled from "styled-components";
+
+type EyeCatchImageProps = {
+  src: string;
+  alt: string;
+};
+
+const EyeCatchImage = ({ src, alt }: EyeCatchImageProps) => {
+  const EyeCatchWrapper = styled.div`
+    margin-top: 20px;
+    margin-bottom: 20px;
+  `;
+  return (
+    <EyeCatchWrapper>
+      <Image src={src} alt={alt} width={800} height={400} />
+    </EyeCatchWrapper>
+  );
+};
+
+export default EyeCatchImage;
