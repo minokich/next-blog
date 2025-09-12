6a0b83137dda312c33e0a06cd3020ba4d807b4cc
minokich <mino0525dev@gmail.com>
2025-09-12 13:13:38 +0900

    Article.tsとして改めて作成

---
+++ src/types/Article.ts
@@ -0,0 +1,24 @@
+export type ArticleType = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  // TODO: Summary（micro CMS側で定義を追加する）
+  // summary: string;
+  eyecatch?: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+};
